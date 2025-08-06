18df956e81849e3f0501984b788db47f53f5f498
Kevin ‎‎‎‏Wu‏‏‎ <kevingrahamwu@gmail.com>
2025-08-06 19:07:44 -0400

    Increase number of mlflow runs

--- src/config.py
+++ src/config.py
@@ -46,7 +46,7 @@ RUN_RETENTION_DAYS = 7
 # Actions (on the free tier) has a cap of 6 hours for the entire script, so I set this number to be
 # lower to ensure the whole daily pipeline will run in <6 hours. This can be increased if I did have
 # a paid tier.
-NUM_EXPERIMENT_EVALS = 1
+NUM_EXPERIMENT_EVALS = 25
 
 # Number of top High Quality clusters to keep
 HQ_CLUSTER_LIMIT = 10
